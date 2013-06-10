d78a0ed32d8163aaeca16d713a8f0c5cf5c69d3d
Alexey Bistriy <alexeybistriy@gmail.com>
2013-06-10 16:44:26 +0400

    test 21/05/2012

--- init.rb
+++ init.rb
@@ -26,7 +26,8 @@ require_relative "Parser"
 require_relative "tree_refs"
 
 debug=true
-
+error=0
+dump=0
 def css_no_nil(li,css_in,attribute_in)
   css=li.css(css_in)
   unless css[0].nil?
@@ -54,13 +55,15 @@ watirff = Watir::Browser.new :ff, :http_client => client
 watirff.goto "www.aliexpress.com"
 html=watirff.html
 
+
+
 # Прсим ссылки бокового меню
 # xpatb для ссылок "//html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[1]/div/dl/dd/dl/dd/a"
 tree_menu=TreeRefs.new
 start_page=Parser.new
 if  watirff.url=="http://www.aliexpress.com/homeRu.htm"
   menu=start_page.navigate_css(html,".cate-list-item")
-   version=1
+  version=1
 elsif watirff.url=="http://www.aliexpress.com/"
   menu=start_page.navigate_xpath(html,"//html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[1]/div/dl/dd/dl/dd/a")
   version=2
@@ -77,59 +80,71 @@ end
 #
 #end
 
-next_href=menu[3][:href]
 
-while next_href!=""
 
-watirff.goto  next_href
+menu.each_index do |index|
+  link=menu[index]
+  next_href=link[:href]
+  next if index < dump
+  while next_href!=""
+    begin
+      watirff.goto  next_href
+    rescue Timeout::Error
+      error+=1
+      File.open('error.txt', 'a'){|file| file.write "error #{error.to_s} no download URL= #{next_href}"}
+      File.open('dump.txt', 'w'){|file| file.write "#{index}:#{link[:href]}:#{next_href}"}
+      dump = index
+    end
+    html=watirff.html
+    puts "<<<<<<<<<<<<<<<<<<<<<<<<<"+link[:href]+">>>>>>>>>>>>>>>>>>>>>>>>>>"
+    page = Nokogiri::HTML(html)
 
-html=watirff.html
-puts "<<<<<<<<<<<<<<<<<<<<<<<<<"+menu[3][:href]+">>>>>>>>>>>>>>>>>>>>>>>>>>"
-page = Nokogiri::HTML(html)
+    #data=[]
+    page.css(".list-item").each do |li|
+      item=Hash.new
+      puts "{{{{{{{{{{{{{{{{{{{{{{{{{{"
+      puts "="+item[:qrdata]=li[:qrdata]
+      puts "="+item[:href_img]=css_no_nil(li,".img a",:href)
+      puts "="+item[:src_img]=css_no_nil(li,".img img",:src)
+      puts "="+item[:href_item]=css_no_nil(li,".product",:href)
+      puts "="+ item[:content_item]=css_no_nil(li,".product","content")
+      puts  "="+item[:title_item]=css_no_nil(li,".product",:title)
+      puts  "="+item[:brief_item]=css_no_nil(li,".brief","content")
+      puts  "="+item[:rate_item]=css_no_nil(li,"span[itemprop='ratingValue']","content")
+      puts "="+ item[:rate_review]=css_no_nil(li,"span[itemprop='reviewCount']","content")
+      puts  "="+item[:rate_feedback]=css_no_nil(li,".rate-num","content")
+      puts  "="+item[:rate_orders]=css_no_nil(li,"em[title='Total Orders']","content")
+      case version
+      when 1
+        puts  "="+item[:currency]=css_no_nil(li,".price-m .currency","content")
+        puts  "="+item[:price]=css_no_nil(li,".price-m .value","content")
+        puts  "="+item[:unit]=css_no_nil(li,".price-m .unit","content")
+      when 2
+        puts  "="+item[:currency]=css_no_nil(li,".price-m span[itemprop='priceCurrency']","content")
+        puts  "="+item[:price]=css_no_nil(li,".price-m span[itemprop='price']","content")
+        puts  "="+item[:unit]=css_no_nil(li,".price-m .unit","content")
+      end
+      #puts  "="+item[:cur]=css_no_nil(li,".pnl-shipping .unit","content")
+      puts  "="+item[:sheeping]=css_no_nil(li,".pnl-shipping .price .value","content")
+      puts  "="+item[:un]=css_no_nil(li,".pnl-shipping .price .unit","content")
 
-#data=[]
-page.css(".list-item").each do |li|
-  item=Hash.new
-  puts "{{{{{{{{{{{{{{{{{{{{{{{{{{"
-  puts "="+item[:qrdata]=li[:qrdata]
-  puts "="+item[:href_img]=css_no_nil(li,".img a",:href)
-  puts "="+item[:src_img]=css_no_nil(li,".img img",:src)
-  puts "="+item[:href_item]=css_no_nil(li,".product",:href)
-  puts "="+ item[:content_item]=css_no_nil(li,".product","content")
-  puts  "="+item[:title_item]=css_no_nil(li,".product",:title)
-  puts  "="+item[:brief_item]=css_no_nil(li,".brief","content")
-  puts  "="+item[:rate_item]=css_no_nil(li,"span[itemprop='ratingValue']","content")
-  puts "="+ item[:rate_review]=css_no_nil(li,"span[itemprop='reviewCount']","content")
-  puts  "="+item[:rate_feedback]=css_no_nil(li,".rate-num","content")
-  puts  "="+item[:rate_orders]=css_no_nil(li,"em[title='Total Orders']","content")
-  case version
-  when 1
-    puts  "="+item[:currency]=css_no_nil(li,".price-m .currency","content")
-    puts  "="+item[:price]=css_no_nil(li,".price-m .value","content")
-    puts  "="+item[:unit]=css_no_nil(li,".price-m .unit","content")
-  when 2
-    puts  "="+item[:currency]=css_no_nil(li,".price-m span[itemprop='priceCurrency']","content")
-    puts  "="+item[:price]=css_no_nil(li,".price-m span[itemprop='price']","content")
-    puts  "="+item[:unit]=css_no_nil(li,".price-m .unit","content")
+      puts  "="+item[:free]=css_no_nil(li,".free-s","content")
+      puts  "="+item[:via]=css_no_nil(li,".pnl-shipping .price","content")
+      puts ")))))))))))))))))))))))))))))))"
+      # сохранение в масив
+      #data.push(item)
+      #запись в файл разделитель;
+
+      File.open(link[:content].gsub(/\&/," ")+'.txt', 'a'){|file| file.write item.values.join("	")+"\n"}
+
+    end
+
+    next_href=css_no_nil(page,".page-next",:href)
+    #next_href=""
   end
-  #puts  "="+item[:cur]=css_no_nil(li,".pnl-shipping .unit","content")
-  puts  "="+item[:sheeping]=css_no_nil(li,".pnl-shipping .price .value","content")
-  puts  "="+item[:un]=css_no_nil(li,".pnl-shipping .price .unit","content")
 
-  puts  "="+item[:free]=css_no_nil(li,".free-s","content")
-  puts  "="+item[:via]=css_no_nil(li,".pnl-shipping .price","content")
-  puts ")))))))))))))))))))))))))))))))"
-  # сохранение в масив
-  #data.push(item)
-  #запись в файл разделитель;
-
-  File.open('sleep2.txt', 'a'){|file| file.write item.values.join(";")+"\n"}
-
+  #if debug
+  #  tree_menu.puts_array_hashes(data)
+  #end
 end
-
-next_href=css_no_nil(page,".page-next",:href)
-next_href=""
-end
-#if debug
-#  tree_menu.puts_array_hashes(data)
-#end
+File.open('dump.txt', 'w'){|file| file.write ""}
