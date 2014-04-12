52e041c723abc9bce9bfaa3674c4a58dc5b8ea44
Alexey Bistriy <alexeybistriy@gmail.com>
2014-04-12 10:40:00 +0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	init.rb
    	parser.rb

--- run.rb
+++ run.rb
@@ -1,48 +1,20 @@
 #coding: utf-8
 
 require_relative './lib/init'
-
 #
 module Robot
-  url='https://www.google.com/search?&client=google-csbe'
-  request='тренинги'
+  url='http://kharkov-online.com/catalog'
   path="./data/"
-  file='google.csv'
-  database='google'
-  table='traning'
-  table2='link'
-
-  key=Key.new
-
-
-   hash_link={'link'=>'VARCHAR(255)'}
-   hash_fields={'firma'=>'VARCHAR(255)',
-               'email'=>'VARCHAR(255)',
-               'host'=>'VARCHAR(255)',
-               'site'=>'VARCHAR(255)',
-               'title'=>'VARCHAR(255)'}
-
-
-  #del_table2 database, table
-  #drop2 database
-  #create2 database
-  #puts '1'
-  #create_table2 database, table2, hash_link
-  #puts '2'
-  #create_table2 database, table2, hash_fields
-  #puts '3'
-  #
-  #record=['myfirma','email','myhost','mysite','mytitle']
-  #insert2 database, table, record
-
-
-  starturi=Addressable::URI.parse('http://www.training.com.ua/').normalize
-  baseuri=Addressable::URI.parse('http://www.training.com.ua/').normalize
-
+  file='kharkov-online.csv'
+#
+#
+#  starturi=Addressable::URI.parse('http://www.trn.ua/companies/').normalize
+#  baseuri=Addressable::URI.parse('http://www.trn.ua/').normalize
+#
   block_record=Record.new
   block_record.name='block'
-  block_record.method='xpath'
-  block_record.key='.//cite[@class="vurls"]'
+  block_record.method='css'
+  block_record.key='div.rubricator-item a'
   block_record.index=nil
   block_record.attribute=nil
 
@@ -51,176 +23,240 @@ module Robot
   href_record.method='attribute'
   href_record.key=nil
   href_record.index=nil
-  href_record.attribute='content'
+  href_record.attribute='href'
 
-  h1_record=Record.new
-  h1_record.name='firma'
-  h1_record.method='css'
-  h1_record.key='h1'
-  h1_record.index=0
-  h1_record.attribute='content'
-
-  h2_record=Record.new
-  h2_record.name='firma'
-  h2_record.method='css'
-  h2_record.key='h2'
-  h2_record.index=0
-  h2_record.attribute='content'
-
-  h3_record=Record.new
-  h3_record.name='firma'
-  h3_record.method='css'
-  h3_record.key='h3'
-  h3_record.index=0
-  h3_record.attribute='content'
+  category_record=Record.new
+  category_record.name='page'
+  category_record.method='attribute'
+  category_record.key=nil
+  category_record.index=nil
+  category_record.attribute='content'
 
 
+  block_record2=Record.new
+  block_record2.name='block2'
+  block_record2.method='css'
+  block_record2.key='div.cat-item'
+  block_record2.index=nil
+  block_record2.attribute=nil
+
+  #href_record2=Record.new
+  #href_record2.name='page2'
+  #href_record2.method='attribute'
+  #href_record2.key=nil
+  #href_record2.index=nil
+  #href_record2.attribute='href'
+
+##
+  firma_record=Record.new
+  firma_record.name='firma'
+  firma_record.method='css'
+  firma_record.key='h2 a'
+  firma_record.index=nil
+  firma_record.attribute='content'
+
+  address_record=Record.new
+  address_record.name='info'
+  address_record.method='xpath'
+  address_record.key='.//ul[@class="catlist-data"]/li[span[text()="Адрес:"]]'
+  address_record.index=nil
+  address_record.attribute='content'
+
+  telephone_record=Record.new
+  telephone_record.name='info'
+  telephone_record.method='xpath'
+  telephone_record.key='.//ul[@class="catlist-data"]/li[span[text()="Тел.:"]]'
+  telephone_record.index=nil
+  telephone_record.attribute='content'
+
+  site_record=Record.new
+  site_record.name='info'
+  site_record.method='xpath'
+  site_record.key='.//ul[@class="catlist-data"]/li[span[text()="Сайт:"]]'
+  site_record.index=nil
+  site_record.attribute='content'
 
 
+
+#
+#  host_record=Record.new
+#  host_record.name='email'
+#  host_record.method='xpath'
+#  host_record.key='.//noindex/a[@target="blank"]'
+#  host_record.index=nil
+#  host_record.attribute='content'
+#
+#  contacts_record=Record.new
+#  contacts_record.name='email'
+#  contacts_record.method='css'
+#  contacts_record.key='a'
+#  contacts_record.index=nil
+#  contacts_record.attribute='content'
+#
+#  href_record4=Record.new
+#  href_record4.name='page2'
+#  href_record4.method='css'
+#  href_record4.key='a'
+#  href_record4.index=nil
+#  href_record4.attribute='href'
+#
+#  email_record=Record.new
+#  email_record.name='css'
+#  email_record.method='a'
+#  email_record.key='.//div[@class="items"]/div[@class="title-link"][2]/a'
+#  email_record.index=nil
+#  email_record.attribute='content'
+
+#
+#  #block_record2=Record.new
+#  #block_record2.name='block2'
+#  #block_record2.method='xpath'
+#  #block_record2.key='.//li/strong/a'
+#  #block_record2.index=nil
+#  #block_record2.attribute=nil
+#  #
+#  #href_record2=Record.new
+#  #href_record2.name='page2'
+#  #href_record2.method='attribute'
+#  #href_record2.key=nil
+#  #href_record2.index=nil
+#  #href_record2.attribute='href'
+#
+#
   loader=Loader.new  #'Windows-1251'
   parser=NokoParser.new
 
   loader2=Loader.new #'Windows-1251'
   parser2=NokoParser.new
-
-  loader3=Loader.new #'Windows-1251'
-  parser3=NokoParser.new
   #
-  pr=Proxy.new
-  pr.count=120
+  #loader3=Loader.new #'Windows-1251'
+  #parser3=NokoParser.new
+  #
+  #loader4=Loader.new #'Windows-1251'
+  #parser4=NokoParser.new
+  #
+  #loader5=Loader.new #'Windows-1251'
+  #parser5=NokoParser.new
+  #
+  k=0
+  loader.goto(url)
+  parser.document(loader.html)
+  page=parser.page
 
-    key.key_search.each do |search|
-      key.key_domains.each do |domain|
-        key.key_positions.each do |position|
+  nodesblocks=parser.nodes_by_record(page,block_record)
+  nodesblocks.each do |node|
+    next_href=parser.attribute_by_record(node,href_record)
+    if next_href=='http://kharkov-online.com/catalog/zoogoods'
+      k=1
+    end
+    if k==1
+      next_href2=next_href+'?page='
+      category=parser.attribute_by_record(node,category_record)
+      #pages=category.scan(/\((\d+)\)/).join.to_i
+      puts next_href
+      puts category
 
-        seach_url=url+"&q=intitle:"+request+"+"+search+"+"+"site:"+domain+"&"+position
-        linker=select2 database, table2, 'link', seach_url
-        unless linker.count==0
-          puts "уже грузили #{seach_url}"
-        else
+      loader2.goto(next_href)
+      parser2.document(loader2.html)
+      page2=parser2.page
 
-          #if pr.count<120
-          #   pr.count+=1
-          #else
-          #  proxy=pr.get_proxy(seach_url)
-          #  pr.count=0
-          #end
-          puts seach_url
-          link=[seach_url]
-          insert2 database, table2, link
-          loader.goto(seach_url)
-          parser.document(loader.html)
-          page=parser.page
-          #puts loader.html
 
-          nodesblocks=parser.nodes_by_record(page,block_record)
-          nodesblocks.each do |node|
-            #puts node.to_s
-            next_href=parser.attribute_by_record(node,href_record)
-            if next_href=~/https:\/\//
-              host=next_href.scan(/^(https:\/\/[^\/]*\/)/).join
-              next_href='https://'+host
-              host.gsub!(/^www\./,'')
-            else
-              host=next_href.scan(/^([^\/]*\/)/).join
-              next_href='http://'+host
-              host.gsub!(/^www\./,'')
+      nodesblocks2=parser2.nodes_by_record(page2,block_record2)
+      nodesblocks2.each do |node2|
+         firma=parser2.attribute_by_record(node2,firma_record)
+         address=parser2.attribute_by_record(node2,address_record)
+         contact=parser2.attribute_by_record(node2,telephone_record)
+         telephone=contact.gsub(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}\b/,'').gsub(/(E|e)-mail:/,'')
+         email=contact.scan(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}\b/).join(';')
+         site=parser2.attribute_by_record(node2,site_record)
+         puts '============================'
+         puts firma
+         puts address
+         puts telephone
+         puts email
+         puts site
+         arr=[category,firma,address,telephone,email,site]
+         unless firma==''
+             CSV.open(path+file, 'a:UTF-8', {:col_sep=>";"})do |line|
+               line << arr
+             end
+         else
+            next_href2=''
+         end
+      end
+      count=0
+      while next_href2!=''
+        count+=1
+        loader2.goto(next_href2+count.to_s)
+        parser2.document(loader2.html)
+        page2=parser2.page
+
+
+        nodesblocks2=parser2.nodes_by_record(page2,block_record2)
+        nodesblocks2.each do |node2|
+          firma=parser2.attribute_by_record(node2,firma_record)
+          address=parser2.attribute_by_record(node2,address_record)
+          contact=parser2.attribute_by_record(node2,telephone_record)
+          telephone=contact.gsub(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}\b/,'').gsub(/(E|e)-mail:/,'')
+          email=contact.scan(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}\b/).join(';')
+          site=parser2.attribute_by_record(node2,site_record)
+          puts '============================'
+          puts firma
+          puts address
+          puts telephone
+          puts email
+          puts site
+          arr=[category,firma,address,telephone,email,site]
+          unless firma==  ''
+            CSV.open(path+file, 'a:UTF-8', {:col_sep=>";"})do |line|
+              line << arr
             end
 
-            puts host
-            selector=select2 database, table, 'host', host
-            if selector.count==0 and host!=''
-              firma=''
-              email=''
-              contact=''
-              title=''
-              begin
-                loader2.goto(next_href)
-                parser2.document(loader2.html)
-                page2=parser2.page
-                title=page2.title.to_s.strip
-
-                #h1=parser.attribute_by_record(page2,h1_record)
-                #h2=parser.attribute_by_record(page2,h2_record)
-                #h3=parser.attribute_by_record(page2,h3_record)
-                #key_word=key.key_words.join('|')
-                #if h1=~/#{key_word}/ui
-                #  firma=h1.strip
-                #elsif h2=~/#{key_word}/ui
-                #  firma=h2.strip
-                #elsif h3=~/#{key_word}/ui
-                #  firma=h3.strip
-                #else
-                #  firma=''
-                #end
-                firma=''
-
-                temp=Addressable::URI.parse(next_href).normalize
-                nodeblock2=page2.css('a')
-                nodeblock2.each do |node2|
-                   if node2.to_s=~/контакт|contact|нас найти|наши координаты/iu
-                        #puts node2.to_s
-                        unless node2[:href].nil?
-                          contact=node2[:href]
-                          contact=temp.join(node2[:href]) unless node2[:href]=~/^http(s)?:\/\//
-                          loader3.goto(contact)
-
-                          if email==''
-                            email=loader3.html.scan(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}\b/).join(';')
-                          else
-                            email=email+';'+loader3.html.scan(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}\b/).join(';')
-                          end
-                        end
-                   end
-
-                end
-
-                mails=email.split(';')
-                mails.uniq!
-                email=mails.join(';')
-
-                puts firma
-                puts email
-                puts host
-                puts contact
-                puts title
-
-                puts '======================================================='
-
-                arr=[firma,email,host,contact.to_str,title]
-
-                record=[]
-                record.push(firma)
-                record.push(email)
-                record.push(host)
-                record.push(contact.to_str)
-                record.push(title)
-                insert2 database, table, record
-
-                puts '++++++++++++++++++++++++++++++++++++++++++++'
-                CSV.open(path+file, 'a:UTF-8', {:col_sep=>";"})do |line|
-                  line << arr
-                end
-              rescue
-                puts 'skip bug host'
-              end
-            end
+          else
+            next_href2=''
           end
-
         end
       end
-    end
+   end
+  #    next_href2=parser2.attribute_by_record(node2,href_record2)
+  #
+  #    loader3.goto(baseuri.join(next_href2))
+  #    parser3.document(loader3.html)
+  #    page3=parser3.page
+  #    host=parser3.attribute_by_record(page3,host_record)
+  #    puts baseuri.join(next_href)
+  #    puts firma
+  #    puts host
+  #    email=''
+  #    contact=''
+  #    unless host.empty?
+  #      loader4.goto(host)
+  #      parser4.document(loader4.html)
+  #      page4=parser4.page
+  #      temp=Addressable::URI.parse(host).normalize
+  #      nodeblock4=page4.css('a')
+  #      nodeblock4.each do |node4|
+  #        if node4.content=~/контакт|contact/ui
+  #          unless node4[:href].nil?
+  #            contact=temp.join(node4[:href])
+  #            loader5.goto(contact)
+  #            parser5.document(loader5.html)
+  #            email=loader5.html.scan(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}\b/).join(';')
+  #          end
+  #        end
+  #
+  #      end
+  #    end
+  #    puts contact
+  #    puts email
+  #    puts '======================================================='
+  #    arr=[firma,email,host,contact]
+  #    puts '++++++++++++++++++++++++++++++++++++++++++++'
+  #    CSV.open(path+file, 'a:UTF-8', {:col_sep=>";"})do |line|
+  #      line << arr
+  #    end
+  #
+
   end
 end
-
-
-
 #
-###
-##
-#file=File.open('c:/temp/google.csv', 'r:UTF-8')
-#tmp=file.read
-#file.close
-#puts tmp
-#tmp.encode!('Windows-1251', invalid: :replace , undef: :replace)
-#File.open('c:/temp/table_google.csv', 'a:Windows-1251'){|file| file.write tmp}
