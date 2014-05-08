e5006bec38a324a95bd16eecc1ac5f7ac72fedc0
Alexey Bistriy <alexeybistriy@gmail.com>
2014-05-08 15:55:12 +0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	parser.rb

---
+++ spec/roboparser/proxer_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Proxer do
+
+  describe '#getproxy' do
+    it 'gives new value'
+    it 'add new proxy record if not is free proxy record'
+  end
+
+  describe '#access' do
+    it 'get '
+  end
+  describe '#verify' do
+    it 'verify available proxy'
+
+  end
+
+end
