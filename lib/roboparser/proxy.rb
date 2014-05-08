f9fc23280d3f46971a28bf549827227eb9b4e1d6
Alexey Bistriy <alexeybistriy@gmail.com>
2014-05-08 15:54:43 +0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	parser.rb

---
+++ lib/roboparser/proxy.rb
@@ -0,0 +1,21 @@
+module Roboparser
+  class Proxy
+    def self.rereade
+
+    end
+    def initialize(uri)
+      @uri=uri
+      @port=uri.port
+
+    end
+
+    def url
+
+    end
+    def port
+
+    end
+    def proto
+
+    end
+  end
